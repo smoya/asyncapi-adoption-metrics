59b6ee8b68baca87a1d64d0c7f1dd5c1547eb406
Sergio Moya <1083296+smoya@users.noreply.github.com>
2023-10-23 11:31:25 +0200

    fix: stdout sink is now printing metadata and type

--- src/sink.ts
+++ src/sink.ts
@@ -8,7 +8,7 @@ export interface Sink {
 export class StdOutSink implements Sink {
   async send(metrics: Metrics): Promise<void> {
     metrics.forEach(metric => {
-      console.log(`${metric.name} ${metric.value}`); // TODO better print
+      console.log('%s\t%s\t%O\t%s', metric.name, metric.type.toUpperCase(), metric.metadata, metric.value);
     });
   }
 }
